d2a1e2b4d5f0e50a86f804c54395b7ef7e08af97
Padmanabh <padmanabhmishra407@gmail.com>
2023-01-10 14:13:05 +0530

    first commit

---
+++ 09-01-2023/HelloWorld.js
@@ -0,0 +1,5 @@
+var user = {
+    name: "Hayes",
+    id: 09
+};
+console.log(user.name);
---
+++ 09-01-2023/HelloWorld.ts
@@ -0,0 +1,14 @@
+interface User {
+    name: string;
+    id: number;
+  }
+  interface  Number: a;
+  const user: User = {
+    name: "Hayes",
+    id: 09,
+  };
+
+console.log(user.name)
+
+var h:number;
+export{}
